a76b5f4a4e2e446ac99b8ba41920d98b2867b79d
animesh-tyagi <animesh.tyagi.720@gmail.com>
2026-04-22 19:31:51 +0000

    fixed typo in about page

--- src/createAbout.js
+++ src/createAbout.js
@@ -10,7 +10,7 @@ function createAbout() {
     frag.appendChild(heading);
     
     const aboutInfo = document.createElement("p");
-    aboutInfo.innerHTML = "tf bro, mind your own business. Ever head of this thing called privacy?";
+    aboutInfo.innerHTML = "tf bro, mind your own business. Ever heard of this thing called privacy?";
     frag.appendChild(aboutInfo);
 
     content.appendChild(frag);
